6a8cb2bf95535e614e6a1a6949e17fe6805bbd7d
cb <jimmybobo123@gmail.com>
2021-12-07 23:35:35 -0800

    Finish algo part

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <unordered_map>
-#include<stdio.h>
-#include<iostream>
-#include<string>
-#include<vector>
-#include <fstream>
+#include <stdio.h>
+
 #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 #include "stringGenerator.h"
 
@@ -102,8 +103,7 @@ vector<int> sapceEfficientAlignment(const string& str1, const string& str2) {
   for (int j = 1; j <= n; j++) {
     DP[1][0] = j * gapPenalty;
     for (int i = 1; i <= m; i++) {
-      int tmp1 = DP[0][i - 1] +
-                 costTable.at(str1[i - 1]).at(str2[j -1]);
+      int tmp1 = DP[0][i - 1] + costTable.at(str1[i - 1]).at(str2[j - 1]);
       int tmp2 = DP[0][i] + gapPenalty;
       int tmp3 = DP[1][i - 1] + gapPenalty;
       if (tmp1 <= tmp2 && tmp1 <= tmp3) {
@@ -124,12 +124,10 @@ vector<int> sapceEfficientAlignment(const string& str1, const string& str2) {
   return DP[1];
 }
 
-int helper(const string& str1, const string& str2,
-                              int start1, int end1, int start2, int end2,
-                              vector<vector<int>>& P) {
-  int m = end1 - start1 + 1;
-  int n = end2 - start2 + 1;
-
+vector<vector<char>> helper(const string& str1, const string& str2,
+                            int& alignCost) {
+  int m = str1.size();
+  int n = str2.size();
   // Small problem solved with basic alignment algorithm
   if (m < 3 || n < 3) {
     vector<vector<int>> DP = vector<vector<int>>(m + 1, vector<int>(n + 1, 0));
@@ -139,81 +137,95 @@ int helper(const string& str1, const string& str2,
     for (int i = 1; i <= n; i++) {
       DP[0][i] = i * gapPenalty;
     }
-    for (int i =  1; i <= m; i++) {
+    for (int i = 1; i <= m; i++) {
       for (int j = 1; j <= n; j++) {
-        int tmp1 = costTable.at(str1[i + start1 - 1]).at(str2[j + start2 - 1]) +
-                   DP[i-1][j-1];
+        int tmp1 = costTable.at(str1[i - 1]).at(str2[j - 1]) + DP[i - 1][j - 1];
         int tmp2 = gapPenalty + DP[i - 1][j];
         int tmp3 = gapPenalty + DP[i][j - 1];
         if (tmp1 <= tmp2 && tmp1 <= tmp3) {
-          DP[i - 1][j - 1] = tmp1;
+          DP[i][j] = tmp1;
           continue;
         }
         if (tmp2 <= tmp1 && tmp2 <= tmp3) {
-          DP[i - 1][j] = tmp2;
+          DP[i][j] = tmp2;
           continue;
         }
         if (tmp3 <= tmp1 && tmp3 <= tmp2) {
-          DP[i][j - 1] = tmp3;
+          DP[i][j] = tmp3;
           continue;
         }
       }
     }
 
-    int l = m + n;
-    int idx1 = l - 1;
-    int idx2 = l - 1;
+    vector<vector<char>> ans;
     int i = m;
     int j = n;
-    P.push_back({end1, end2});
-    while (i != 0 && j != 0) {
-      if (DP[i - 1][j - 1] +
-              costTable.at(str1[i + start1 - 1]).at(str2[j + start2 - 1]) ==
-          DP[i][j]) {
+    while (i > 0 || j > 0) {
+      if (j && DP[i][j - 1] + gapPenalty == DP[i][j]) {
+        ans.push_back({'_', str2[j - 1]});
+        j--;
+      } else if (i && j &&
+                 DP[i - 1][j - 1] + costTable.at(str1[i - 1]).at(str2[j - 1]) ==
+                     DP[i][j]) {
+        ans.push_back({str1[i - 1], str2[j - 1]});
         i--;
         j--;
-      } else if (DP[i - start1][j - start2 - 1] + gapPenalty ==
-                 DP[i - start1][j - start2]) {
-        j--;
-      } else {
+      } else if (i && DP[i - 1][j] + gapPenalty == DP[i][j]) {
+        ans.push_back({str1[i - 1], '_'});
         i--;
       }
-      P.push_back({i + start1 - 1, j + start2 - 1});
     }
-    return DP[m][n];
-  }
-  
-  vector<int> forwardCost = sapceEfficientAlignment(str1, str2.substr(start2, n/2));
 
-  string tmp1 = str1.substr(start1, m);
-  string tmp2 = str2.substr(start2, n);
+    reverse(ans.begin(), ans.end());
+
+    alignCost = DP[m][n];
+    return ans;
+  }
+
+  vector<int> forwardCost =
+      sapceEfficientAlignment(str1, str2.substr(0, n / 2));
+
+  string tmp1 = str1;
+  string tmp2 = str2.substr(n / 2, n - n / 2);
   reverse(tmp1.begin(), tmp1.end());
   reverse(tmp2.begin(), tmp2.end());
   vector<int> backwardCost = sapceEfficientAlignment(tmp1, tmp2);
 
   int q = -1;
-  int minCost = INT16_MAX; 
+  int minCost = INT16_MAX;
   for (int i = 1; i <= m; i++) {
-    if (forwardCost[i] + backwardCost[i] < minCost) {
-      minCost = forwardCost[i] + backwardCost[i];
+    if (forwardCost[i] + backwardCost[m - i] < minCost) {
+      minCost = forwardCost[i] + backwardCost[m - i];
       q = i;
     }
   }
-  q = q + start1 - 1;
+  alignCost = minCost;
 
-  return minCost +
-         helper(str1, str2, start1, start1 + q, start2, start2 + n / 2, P) +
-         helper(str1, str2, start1 + q + 1, end1, start2 + n / 2 + 1, end2, P);
+  vector<vector<char>> left =
+      helper(str1.substr(0, q), str2.substr(0, n / 2), minCost);
+  vector<vector<char>> right =
+      helper(str1.substr(q), str2.substr(n / 2), minCost);
+
+  vector<vector<char>> ans;
+  ans.insert(ans.end(), left.begin(), left.end());
+  ans.insert(ans.end(), right.begin(), right.end());
+
+  return ans;
 }
 
 int devideAndConquerAlignment(const string& str1, const string& str2,
                               string& newStr1, string& newStr2) {
   int m = str1.size();
   int n = str2.size();
-  vector<vector<int>> path;
-  int cost = helper(str1, str2, 0, m - 1, 0, n - 1, path);
+  int cost;
+  vector<vector<char>> M = helper(str1, str2, cost);
 
-  // costtruct new string from path
+  newStr1 = "";
+  newStr2 = "";
+  for (auto& m : M) {
+    newStr1 = newStr1 + m[0];
+    newStr2 = newStr2 + m[1];
+  }
 
   return cost;
 }
@@ -232,27 +244,59 @@ int checkAlignment(const string& str1, const string& str2) {
   return ans;
 }
 
-int main() {
-  stringGenerator sg1("BaseTestcases_CS570FinalProject/input1.txt");
-  stringGenerator sg2("BaseTestcases_CS570FinalProject/input2.txt");
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    cout << "Please secify algorithm type (Basic / D&C) and input file" << endl;
+    return -1;
+  }
+  string filename = argv[2];
+  string type = argv[1];
 
-  string new1, new2, newa, newb;
+  stringGenerator sg(filename);
 
-  string str1 = sg1.getFirstString();
-  string str2 = sg1.getSecondString();
-  int ans1 = devideAndConquerAlignment(str1, str2, new1, new2);
-  cout << "Ans1: " << ans1 << endl;
-  cout << "new1: " << new1 << endl;
-  cout << "new2: " << new2 << endl;
-  cout << "checkAlignment(new1, new2): " << checkAlignment(new1, new2) << endl;
+  string str1 = sg.getFirstString();
+  string str2 = sg.getSecondString();
+  string new1, new2;
 
-  string stra = sg2.getFirstString();
-  string strb = sg2.getSecondString();
-  int ans2 = devideAndConquerAlignment(stra, strb, newa, newb);
-  cout << "Ans2: " << ans2 << endl;
-  cout << "newa: " << newa << endl;
-  cout << "newb: " << newb << endl;
-  cout << "checkAlignment(newa, newb): " << checkAlignment(newa, newb) << endl;
+  if (type == "Basic") {
+    int ans1 = basicAlignment(str1, str2, new1, new2);
+    cout << "Ans1: " << ans1 << endl;
+    cout << "new1: " << new1 << endl;
+    cout << "new2: " << new2 << endl;
+    cout << "checkAlignment(new1, new2): " << checkAlignment(new1, new2)
+         << endl;
+  } else if (type == "D&C") {
+    int ans1 = devideAndConquerAlignment(str1, str2, new1, new2);
+    cout << "Ans1: " << ans1 << endl;
+    cout << "new1: " << new1 << endl;
+    cout << "new2: " << new2 << endl;
+    cout << "checkAlignment(new1, new2): " << checkAlignment(new1, new2)
+         << endl;
+  } else {
+    cout << "Unkown algorithm type" << endl;
+  }
 
   return 0;
+  // stringGenerator sg1("BaseTestcases_CS570FinalProject/input1.txt");
+  // stringGenerator sg2("BaseTestcases_CS570FinalProject/input2.txt");
+
+  // string new1, new2, newa, newb;
+  // string a,b,c,d;
+
+  // string str1 = sg1.getFirstString();
+  // string str2 = sg1.getSecondString();
+  // int ans1 = devideAndConquerAlignment(str1, str2, new1, new2);
+  // cout << "Ans1: " << ans1 << " " << basicAlignment(str1, str2, a, b) <<
+  // endl; cout << "new1: " << new1 << endl; cout << "new2: " << new2 << endl;
+  // cout << "checkAlignment(new1, new2): " << checkAlignment(new1, new2) <<
+  // endl;
+
+  // string stra = sg2.getFirstString();
+  // string strb = sg2.getSecondString();
+  // int ans2 = devideAndConquerAlignment(stra, strb, newa, newb);
+  // cout << "Ans2: " << ans2 << " " << basicAlignment(stra, strb, c, d) <<endl;
+  // cout << "newa: " << newa << endl;
+  // cout << "newb: " << newb << endl;
+  // cout << "checkAlignment(newa, newb): " << checkAlignment(newa, newb) <<
+  // endl;
 }
--- stringGenerator.h
+++ stringGenerator.h
@@ -19,10 +19,6 @@ class stringGenerator{
             if(line[line.size() - 1] == 13){
                 line = line.substr(0, line.size()-1);
             }
-            // cout << line << endl;
-            // for(int i = 0; i < line.size(); i++){
-            //     cout << line[i] - 'A' << endl; 
-            // }
             if(jumpIdx1.empty() && str1 == ""){
                 str1 = line;      
                 continue;
@@ -38,28 +34,17 @@ class stringGenerator{
             jumpIdx2.push_back(stoi(line));
         }
 
-        // cout << "string 1 is: " << str1 << "*" <<endl;
-        // for(int a : jumpIdx1){
-        //     cout << a << " ";
-        // }
-        // cout << endl;
-        // cout << "string 2 is: " << str2 << "*" <<endl;
-        // for(int a : jumpIdx2){
-        //     cout << a << " ";
-        // }
-        // cout << endl;
-
         for(int a : jumpIdx1){
             string tmp = str1;
             str1 = tmp.substr(0, a+1) + tmp + tmp.substr(a+1);
         }
-        cout << str1 << endl;
+
 
         for(int a : jumpIdx2){
             string tmp = str2;
             str2 = tmp.substr(0, a+1) + tmp + tmp.substr(a+1);
         }
-        cout << str2 << endl;
+
     }
 
     string getFirstString(){
